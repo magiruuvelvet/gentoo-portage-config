fdc8077eaf77fdcba3f6931b2212141a7f6db1fe
magiruuvelvet <magiruuvelvet@naver.com>
2019-08-31 17:09:19 +0200

    2019-08-31-17:09

---
+++ unittests/macro-tests/macros.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/* current output (2019-08-31, LLVM 9.0.0)
+ *
+ * __GLIBC__
+ * __clang__
+ *
+ */
+
+static inline void print_macro(const char *macro)
+{
+    printf("%s\n", macro);
+}
+
+int main(void)
+{
+    // if this doesn't compile I fucked up when updating my toolchain to a newer version
+    const char *asm = "this is not a ISO C keyword, but a GNU language extension";
+
+// my current libc runtime: declared
+#ifdef __GLIBC__
+    print_macro("__GLIBC__");
+#endif
+
+// my toolchain: not declared
+#ifdef __GNUG__
+    print_macro("__GNUG__");
+#endif
+
+// my toolchain: declared
+#ifdef __clang__
+    print_macro("__clang__");
+#endif
+
+// my toolchain: not declared
+#ifdef _GNU_SOURCE
+    print_macro("_GNU_SOURCE");
+#endif
+
+    return 0;
+}
