1c003af51021ca98613af9c501ce5566e370b819
magiruuvelvet <magiruuvelvet@naver.com>
2022-02-25 19:28:22 +0100

    2022-02-25-19:28

--- unittests/rust-test/test.rs
+++ unittests/rust-test/test.rs
@@ -7,8 +7,21 @@ use std::env;
 // should contain libunwind by default
 // application should work too without crashing
 
+// rust-bin uses libgcc_s but a symlink to libunwind inside
+// /opt/rust-bin-$VERSION/lib/rustlib/x86_64-unknown-linux-gnu/lib
+// libgcc_s.so -> /usr/lib64/libunwind.so
+// makes rustc link binaries to libunwind.so (verify with ldd)
+// and they seem to work
+
+fn test() -> i32
+{
+    println!("test");
+    return 1;
+}
+
 fn main()
 { 
+    println!("{}", test());
     for argument in env::args()
     {
         println!("{}", argument); 
