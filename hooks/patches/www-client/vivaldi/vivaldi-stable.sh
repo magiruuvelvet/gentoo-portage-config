f4a8f9abb9b73283973f2bd9ee05fef76a6c0f02
magiruuvelvet <magiruuvelvet@naver.com>
2024-03-05 15:35:35 +0100

    2024-03-05-15:35 [swap GPU layout]
    depends on https://github.com/magiruuvelvet/gentoo-overlay/commit/38f1c40cef4d4b30e73e6bd27b76bd489a2ad692

--- hooks/patches/www-client/vivaldi/vivaldi-stable.sh
+++ hooks/patches/www-client/vivaldi/vivaldi-stable.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# NOTE: I swapped GPUs in the UEFI settings, dGPU default, primerun -> iGPU
 # use dGPU for rendering and video decoding to take away usage from the iGPU
-exec primerun /opt/vivaldi/vivaldi "$@"
+#exec primerun /opt/vivaldi/vivaldi "$@"
+exec /opt/vivaldi/vivaldi "$@"
